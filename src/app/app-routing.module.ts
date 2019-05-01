f42c8ab0ad9efb252c2695afa7325b16165df4f8
Kaleo A. Piasse <kaleopiasse@gmail.com>
2019-04-30 21:47:29 -0300

    Start projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+
+const routes: Routes = [
+  { path: 'body', component: BodyComponent }
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
